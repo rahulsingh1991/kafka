cf9030c5edb2a0c13be252290ceb33b038c57260
Rahul Kumar Singh <rahulkr.gkp@gmail.com>
2024-10-18 01:56:58 +0530

    Retry support in Consumer

--- src/main/java/com/connect/spring/kafka/config/AbstractKafkaConsumerConfig.java
+++ src/main/java/com/connect/spring/kafka/config/AbstractKafkaConsumerConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +25,7 @@ public abstract class AbstractKafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootStrapServer());
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         populateConsumerSpecificProp(props);
         return new DefaultKafkaConsumerFactory<>(props);
     }
@@ -32,6 +36,16 @@ public abstract class AbstractKafkaConsumerConfig {
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.setConcurrency(1);
+        factory.setBatchListener(true);
+
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        // Set up retry handling
+        // Configure the retry behavior with DefaultErrorHandler
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new FixedBackOff(1000L, 3L) // Retry every 1 second, up to 3 times
+        );
+        factory.setCommonErrorHandler(errorHandler);
         return factory;
     }
 
--- src/main/java/com/connect/spring/kafka/listeners/EmployeeEventListener.java
+++ src/main/java/com/connect/spring/kafka/listeners/EmployeeEventListener.java
@@ -1,8 +1,10 @@
 package com.connect.spring.kafka.listeners;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class EmployeeEventListener {
@@ -12,8 +14,21 @@ public class EmployeeEventListener {
             groupId = "#{kafkaTopicConfig.employeeTopicConsumerGroupId}",
             containerFactory = "kafkaListenerContainerFactory"
     )
-    public void listen(String message) {
-        System.out.println("Received message: " + message);
+    public void listen(List<String> messages, Acknowledgment acknowledgment) {
+        try {
+            // Process each message
+            messages.forEach(message -> {
+                System.out.println("Processing message: " + message);
+            });
+
+            // Manually commit offsets after successfully processing the batch
+            acknowledgment.acknowledge();
+            System.out.println("Offsets have been acknowledged.");
+        } catch (Exception e) {
+            // Handle exception, decide what to do with unprocessed messages may
+            System.err.println("Error processing batch: " + e.getMessage());
+        }
+
     }
 
 }
